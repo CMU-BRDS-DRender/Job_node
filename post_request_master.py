cad4bddf527118c1a5a147fe38b2df2edb6e7f52
varshaprasad96 <44209878+varshaprasad96@users.noreply.github.com>
2018-10-30 19:55:55 -0400

    Update post_request_master.py

--- post_request_master.py
+++ post_request_master.py
@@ -14,3 +14,7 @@ def testPost():
 		end_frame = request.json['endFrame']
 		action = request.json['action']
 		bucket_url = request.json['outputURI']
+		
+os.system("blender -b /XXXXXXXX/scene-Helicopter-27.blend -o /XXXXXXX -f 'start_frame' -a")
+
+#os.system("blender -b /mnt/c/Users/Mihir/Downloads/Demo_274/scene-Helicopter-27.blend -o /mnt/c/Users/Mihir/Downloads/Demo_274/Rendered -f 1 -a")
