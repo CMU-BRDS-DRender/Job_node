e62488a120387ab8797e9ccc90ab2fb11ebc18c5
varshaprasad96 <44209878+varshaprasad96@users.noreply.github.com>
2018-10-30 20:30:34 -0400

    Update upload_file.py

--- upload_file.py
+++ upload_file.py
@@ -8,4 +8,4 @@ s3 = boto3.resource('s3',
 					aws_access_key_id = 'XXXXXXXXXXXXXXXXXXX',
 					aws_secret_access_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
 
-s3.Bucket('renderframe').upload_file('home/Rendered/butterfly (1).jpg', 'renderedframe_0001')
+s3.Bucket('renderframe').upload_file('/home/Blender/Rendered/rendered_001', 'renderedframe_0001')
