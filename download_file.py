e411e95d92e11b125ab1093c19cbc60ca092093a
varshaprasad96 <44209878+varshaprasad96@users.noreply.github.com>
2018-10-30 20:21:19 -0400

    Update download_file.py

--- download_file.py
+++ download_file.py
@@ -2,10 +2,15 @@ import os
 import boto3
 import botocore
 
+directroy = '/home/Blender'
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+
 
 
 s3 = boto3.resource('s3',
 					aws_access_key_id = 'XXXXXXXXXXXXXXXXXXX',
 					aws_secret_access_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
 
-s3.Bucket('renderframe').download_file('/home/Blender/butterfly (1).jpg', 'to_be_rendered_frame_0001')
+s3.Bucket('renderframe').download_file('butterfly (1).jpg', '/home/Blender/to_be_rendered_frame_0001')
