088f0f7493646ee4ae37c7c3561abb207e865f7b
varshaprasad96 <44209878+varshaprasad96@users.noreply.github.com>
2018-10-31 00:24:24 -0400

    Update post_request_master.py

--- post_request_master.py
+++ post_request_master.py
@@ -7,7 +7,7 @@ directory = '/home/Blender/Rendered'
 
 @app.route('/start', methods=['POST'])
 def testPost():
-	if request.method == 'POST':
+	if (request.method == 'POST'):
 		ID = request.json['ID']
 		project_ID = request.json['projectID']
 		source_url = request.json['source']
@@ -15,8 +15,15 @@ def testPost():
 		end_frame = request.json['endFrame']
 		action = request.json['action']
 		bucket_url = request.json['outputURI']
+		return jsonify(start_frame)
 		if not os.path.exists(directory):
     			os.makedirs(directory)
+			
+	if (request.method == 'GET'):
+		return "Status: 200 OK!"
+if __name__ == '__main__':
+	app.run(host = '127.0.0.1', port = 8080, debug = True)
+		
 		
 		os.system("blender -b /home/Blender/to_be_rendered.blend -o /home/Blender/Rendered/rendered_001 -f 1 -a")
 		
