98e8d22d29c8a74ba7434bef1d27d72c1013ad57
varshaprasad96 <44209878+varshaprasad96@users.noreply.github.com>
2018-11-02 19:02:34 -0400

    Update Send_status.py

--- Send_status.py
+++ Send_status.py
@@ -4,3 +4,6 @@ app = Flask(__name__)
 @app.route('/start', methods=['POST'])
 def status_to_master():
     return 'Frame sent for rendering'
+
+if __name__ == '__main__':
+	app.run(host = '127.0.0.1', port = 8080, debug = True)
