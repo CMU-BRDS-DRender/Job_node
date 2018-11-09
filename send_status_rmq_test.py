28c80b1cc8f6c974e04fe433407fb8736277d87a
varshaprasad96 <44209878+varshaprasad96@users.noreply.github.com>
2018-11-08 23:52:18 -0500

    Add files via upload

---
+++ send_status_rmq_test.py
@@ -0,0 +1,36 @@
+# tested only for sending single message on Win
+
+class StatusMessages:
+	def __init__(self, masterIP, queue_name, exchange, routing_key, jobId, frame_number):
+		self.masterIP = 'localhost'
+		self.queue_name = 'StatusQueue'
+		self.exchange = ''
+		self.routing_key = 'StatusQueue'
+		self.jobId = 10
+		self.frame_number = 5
+
+	def send_status(self):
+		connection = pika.BlockingConnection(pika.ConnectionParameters(self.masterIP))
+		channel = connection.channel()
+		channel.queue_declare(queue = self.queue_name)		
+		channel.basic_publish(exchange=self.exchange, routing_key = self.routing_key, body = str(self.jobId) + str(self.frame_number))
+		print("[x] Sent 'Alive' ")
+		connection.close()
+
+#message = new StatusMessages
+#message.send_status()
+
+StatusMessages.send_status()
+
+
+
+#import pika
+
+#connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+#channel = connection.channel()
+#channel.queue_declare(queue='hello')
+#channel.basic_publish(exchange='',
+#                      routing_key = 'hello',
+#                      body = 'Alive')
+#print("[x] Sent 'Alive' ")
+#connection.close()
