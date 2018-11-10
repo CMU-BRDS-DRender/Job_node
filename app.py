eb7f6d9d8217b260e46fb248188fa43c3b8cde4f
Mihir <mihirdphalak@gmail.com>
2018-11-10 01:12:32 -0500

    Everything Done!

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def jobNodeStatus():
     return jsonify(response)
 
 if __name__ == '__main__':
-	app.run(host = '127.0.0.1', port = 8080, debug = True )
+	app.run(host = '0.0.0.0', port = 8080, debug = True )
